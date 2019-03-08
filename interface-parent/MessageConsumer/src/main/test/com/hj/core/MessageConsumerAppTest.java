80d0527e3dacfa930c9b443177da6634639fb363
Harish Janardhanan <harish007007@gmail.com>
2019-03-08 13:11:02 +0000

    skeleton test

---
+++ interface-parent/MessageConsumer/src/main/test/com/hj/core/MessageConsumerAppTest.java
@@ -0,0 +1,14 @@
+package com.hj.core;
+
+import org.junit.jupiter.api.Test;
+
+class MessageConsumerAppTest {
+
+    MessageConsumerApp messageConsumerApp = new MessageConsumerApp();
+
+    @Test
+
+    void testSample() {
+    }
+
+}
